6b5d77459f414c9e1f2e9dce59419bfbd0eecb75
mohammed kullab <moh.kull200@gmail.com>
2024-05-01 21:56:29 +0300

    Edit stories folder structure

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from "components";
+import { Button } from ".";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
