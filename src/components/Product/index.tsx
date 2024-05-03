37ae7ab036b11a5700c5009a25b72ce573a688f0
mohammed kullab <moh.kull200@gmail.com>
2024-05-03 19:01:17 +0300

    edits on product component

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -5,14 +5,23 @@ import Link from "next/link";
 import Card from "components/Card";
 import StarRating from "components/StarRating";
 
-const Product = ({ productData, link = "#" }: ProductProps) => {
+export const Product = ({ productData, link = "#" }: ProductProps) => {
   const { image, price, rating, title } = productData;
   return (
     // <Link href={link}>
     <Card className=" mx-4 my-2 border border-[#ddd] transition-all cursor-pointer p-0">
       <div className="">
         <div className="relative flex items-center justify-center aspect-square md:p-4 ">
-          <Image src={image} alt={title} width={280} height={357} />
+          {image ? (
+            <Image src={image} alt={title} width={280} height={357} />
+          ) : (
+            <Image
+              src="/assets/img/product_placeholder.jpg"
+              alt={title}
+              width={280}
+              height={357}
+            />
+          )}
           <div className="absolute flex items-center justify-center w-full h-full rounded-lg rounded-b-none hover:backdrop-brightness-75"></div>
         </div>
         <div className="p-4 text-center product-details">
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,29 @@
 import { NextPage } from "next";
 
-import type {
-    ReactElement,
-    ReactNode,
-} from "react";
-
+import type { ReactElement, ReactNode } from "react";
 
 export type Children = ReactNode;
 
 export interface MainLayoutProps {
-    children: Children;
-    title: string
-    pageDescription?: string;
-    mainClassName?: string;
-
+  children: Children;
+  title: string;
+  pageDescription?: string;
+  mainClassName?: string;
 }
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-    getNestedLayout?: (page: ReactElement) => ReactNode;
-    mainLayoutProps?: Omit<MainLayoutProps, "children">;
+  getNestedLayout?: (page: ReactElement) => ReactNode;
+  mainLayoutProps?: Omit<MainLayoutProps, "children">;
 };
 
-export type productRatingType = { rate: number, count: number }
-export interface ProductType { id: number, title: string, price: number, description: string, category: string, image: string, rating: productRatingType }
-export type ProductProps = { productData: ProductType, link?: string }
+export type productRatingType = { rate: number; count: number };
+export interface ProductType {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image?: string;
+  rating: productRatingType;
+}
+export type ProductProps = { productData: ProductType; link?: string };
