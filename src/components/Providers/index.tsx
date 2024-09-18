4f67c21a2fc0b64de21ee40179ccad71e71940e5
mohammed kullab <moh.kull200@gmail.com>
2024-09-18 20:03:01 +0300

    added ReactQueryDevtools

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -2,9 +2,15 @@
 import React from "react";
 import { Children } from "types";
 import { TanstackQueryProvider } from "./TanstackQuery";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Providers = ({ children }: { children: Children }) => {
-  return <TanstackQueryProvider>{children}</TanstackQueryProvider>;
+  return (
+    <TanstackQueryProvider>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </TanstackQueryProvider>
+  );
 };
 
 export default Providers;
